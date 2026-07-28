2c82e9dcfe34c8e19d74283f06cc25901cb94619
Piyush Jadhav <piyushjadhavyt@gmail.com>
2026-07-28 18:38:51 +0530

    Data Routing

---
+++ day15-data-routing/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Services from "../pages/Services";
+
+const AppRoutes = () => {
+  let router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/services", element: <Services /> },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
