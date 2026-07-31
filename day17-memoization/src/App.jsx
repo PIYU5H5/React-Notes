ee532c896baab5d0b370c9bed4750341aaa056f1
Piyush Jadhav <piyushjadhavyt@gmail.com>
2026-07-31 09:48:45 +0530

    completed memoisation

--- day17-memoization/src/App.jsx
+++ day17-memoization/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 
@@ -7,13 +7,22 @@ const App = () => {
   const [user, setUser] = useState({ name: "Piyush", id: 555 });
   let greet = useCallback(() => {
     console.log("Hey good ev");
-  },[]);
+  }, []);
+  let calculation = useMemo(() => {
+    console.log("calculation running");
+    let sum = 0;
 
+    for (let i = 0; i < 10000000; i++) {
+      sum += i;
+    }
+    return sum;
+  }, []);
   return (
     <div>
       <h1>Memoization</h1>
       <h2>Count is {count}</h2>
       <h2>name is {user}</h2>
+      <h2>my calculation is {calculation}</h2>
       <button onClick={() => setUser({ ...user, name: "yo" })}>
         Change Name
       </button>
